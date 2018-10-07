8bd95e3ff2f9028b76887d9705e2318ceee99fa3
Ricardo Silva Tavares de Lemos <rtl00@yahoo.com>
2018-10-06 22:00:52 -0700

    Adding etopo path to data

--- R/data.R
+++ R/data.R
@@ -6,3 +6,18 @@
 #'         etopo30[1,2] to 89.75N, -179.25E, and so on.
 #' @source \url{https://data.europa.eu/euodp/data/dataset/data_world-digital-elevation-model-etopo5}
 "etopo30"
+
+#' ETOPO30 bathymetry path
+#'
+#' An array connecting oceanic points, using the Surface Filling Curve algorithm in this package
+#'
+#' @format A data frame with 175469 rows and 3 columns: latitude, longitude, and path ID number
+#' @source The path and the lat/lon can be created afresh by typing
+#'         > data = preprocessBathymetry(etopo30, neritic = -2000, verbose = TRUE)
+#'         > lat = seq(89.75, -89.75, by=-0.5)
+#'         > lon = seq(-179.75, 179.75, by=0.5)
+#'         > sEtopo = sfc(data = data, lat = lat, lon = lon, verbose = TRUE)
+#'         > etopoPath = data.frame(lat = lat[(sEtopo$path - 1) %% 360 + 1],
+#'         >                        lon = lon[floor((sEtopo$path - 1) / 360) + 1],
+#'         >                        path = sEtopo$path)
+"etopoPath"
